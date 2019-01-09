391e1a096b8df5f5da61aed308b14b8279c792e6
Maoyui <3159802583@qq.com>
2019-01-09 21:45:41 +0800

    test

---
+++ src/main.js
@@ -0,0 +1,79 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import axios from 'axios'
+import Vuex from 'vuex'
+
+import infiniteScroll from  'vue-infinite-scroll'
+import {currency} from './util/currency'
+
+import './assets/css/base.css'
+import './assets/css/checkout.css'
+import './assets/css/login.css'
+import './assets/css/product.css'
+
+Vue.use(infiniteScroll);
+Vue.use(Vuex);
+
+Vue.filter("currency",currency);  //全局过滤器
+Vue.config.productionTip = false;
+
+
+
+const store = new Vuex.Store({
+  state: {
+    nickName:'',   //用户名
+    cartCount:0    //购物车数量
+  },
+  mutations: {
+    //更新用户信息
+    updateUserInfo(state, nickName) {
+      state.nickName = nickName;
+    },
+    updateCartCount(state,cartCount){
+      state.cartCount += cartCount;
+    },
+    cleanCartCount(state){
+      state.cartCount = 0
+    }
+  }
+});
+
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  store,
+  router,
+  mounted(){
+    this.checkLogin();
+    this.getCartCount();
+  },
+  methods:{
+    checkLogin(){
+      axios.get("users/checkLogin").then(res=> {
+        var res = res.data;
+        if (res.status == "0") {
+          this.$store.commit("updateUserInfo", res.result);
+        }else{
+          if(this.$route.path!="/goods"){
+            this.$router.push("/goods")
+          }
+        }
+      });
+    },
+    getCartCount(){
+      axios.get("users/getCartCount").then(res=>{
+        var res = res.data;
+        if(res.status=="0"){
+          this.$store.commit("updateCartCount",res.result);
+        }
+      });
+    }
+  },
+  template: '<App/>',
+  //render: h => h(App),
+  components: { App } 
+});//.$mount('#app')
